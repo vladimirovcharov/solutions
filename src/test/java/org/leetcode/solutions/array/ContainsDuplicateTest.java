04cadfcfd19a3489b2ade321cff4473be8fbeefd
Volodymyr Ovcharov <vladimir.1527@gmail.com>
2024-03-02 15:42:15 -0600

    Add tests

--- src/test/java/org/leetcode/solutions/array/ContainsDuplicateTest.java
+++ src/test/java/org/leetcode/solutions/array/ContainsDuplicateTest.java
@@ -14,6 +14,19 @@ class ContainsDuplicateTest {
 
     @Test
     void shouldReturnFalse() {
+        assertFalse(ContainsDuplicate.containsDuplicate(new int[]{}));
         assertFalse(ContainsDuplicate.containsDuplicate(new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    void shouldReturnTrue2() {
+        assertTrue(ContainsDuplicate.containsDuplicate2(new int[]{1, 2, 3, 1}));
+        assertTrue(ContainsDuplicate.containsDuplicate2(new int[]{1, 1, 3, 3, 4, 3, 2, 4, 2}));
+    }
+
+    @Test
+    void shouldReturnFalse2() {
+        assertFalse(ContainsDuplicate.containsDuplicate2(new int[]{}));
+        assertFalse(ContainsDuplicate.containsDuplicate2(new int[]{1, 2, 3, 4}));
     }
 }
