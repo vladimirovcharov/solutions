3cde62c536b961c1b0b3c1572e70a458b15f4702
Volodymyr Ovcharov <vladimir.1527@gmail.com>
2024-03-16 16:54:10 -0500

    Add numbers of the tasks

--- src/main/java/org/solutions/leetcode/array/TwoSum1.java
+++ src/main/java/org/solutions/leetcode/array/TwoSum1.java
@@ -3,7 +3,7 @@ package org.solutions.leetcode.array;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TwoSum {
+public class TwoSum1 {
 
     public static int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
